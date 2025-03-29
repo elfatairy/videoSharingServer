8e0cccada910f586686cad5e4525e4dacc2444b5
OmarHassan <omar13102005@gmail.com>
2025-03-29 18:10:46 +0200

    fix potential js comments

--- api/index.js
+++ api/index.js
@@ -6,19 +6,31 @@ async function getVideoData(videoId) {
   try {
     const response = await fetch(`https://server.infotik.co/posts/${videoId}`);
     if (!response.ok) {
-      return { title: "Video not found", description: "Video not found", thumbnail: "" };
-  }
-  const jsonData = await response.json();
-  if (jsonData && jsonData.statusCode === 200) {
-    return {
-      title: jsonData.data.user?.displayName ?? "Infotik",
-      description: jsonData.data.description,
-      thumbnail: getThumbUrl(jsonData.data.thumbnailObjectName),
-    };
+      return {
+        title: "Video not found",
+        description: "Video not found",
+        thumbnail: "",
+      };
     }
-    return { title: "Video not found", description: "Video not found", thumbnail: "" };
+    const jsonData = await response.json();
+    if (jsonData && jsonData.statusCode === 200) {
+      return {
+        title: jsonData.data.user?.displayName ?? "Infotik",
+        description: jsonData.data.description,
+        thumbnail: getThumbUrl(jsonData.data.thumbnailObjectName),
+      };
+    }
+    return {
+      title: "Video not found",
+      description: "Video not found",
+      thumbnail: "",
+    };
   } catch (error) {
-    return { title: "Video not found", description: "Video not found", thumbnail: "" };
+    return {
+      title: "Video not found",
+      description: "Video not found",
+      thumbnail: "",
+    };
   }
 }
 
@@ -26,19 +38,31 @@ async function getPulseData(videoId) {
   try {
     const response = await fetch(`https://server.infotik.co/pulse/${videoId}`);
     if (!response.ok) {
-      return { title: "Video not found", description: "Video not found", thumbnail: "" };
-  }
-  const jsonData = await response.json();
-  if (jsonData && jsonData.statusCode === 200) {
-    return {
-      title: jsonData.data.user?.displayName ?? "Infotik",
-      description: jsonData.data.description,
-      thumbnail: getThumbUrl(jsonData.data.thumbnailObjectName),
-    };
+      return {
+        title: "Video not found",
+        description: "Video not found",
+        thumbnail: "",
+      };
     }
-    return { title: "Video not found", description: "Video not found", thumbnail: "" };
+    const jsonData = await response.json();
+    if (jsonData && jsonData.statusCode === 200) {
+      return {
+        title: jsonData.data.user?.displayName ?? "Infotik",
+        description: jsonData.data.description,
+        thumbnail: getThumbUrl(jsonData.data.thumbnailObjectName),
+      };
+    }
+    return {
+      title: "Video not found",
+      description: "Video not found",
+      thumbnail: "",
+    };
   } catch (error) {
-    return { title: "Video not found", description: "Video not found", thumbnail: "" };
+    return {
+      title: "Video not found",
+      description: "Video not found",
+      thumbnail: "",
+    };
   }
 }
 
@@ -82,44 +106,38 @@ app.get("/video/:videoId", async (req, res) => {
     </head>
 
     <body>
-        Loading...
+      Loading...
 
-        <script>
-            function detectDevice() {
-                const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      <script>
+        function detectDevice() {
+          const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-                if (/android/i.test(userAgent)) {
-                    return 'android';
-                }
+          if (/android/i.test(userAgent)) {
+              return 'android';
+          }
 
-                if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-                    return 'ios';
-                }
+          if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+              return 'ios';
+          }
 
-                return 'web';
+          return 'web';
+        }
+
+        function redirectBasedOnDevice() {
+            const deviceType = detectDevice();
+
+            if (deviceType === 'android') {
+                window.location.href = \`intent://video/${videoId}#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
+            } else if (deviceType === 'web') {
+                // Redirect web users to the website
+                window.location.href = 'https://www.infotik.co';
             }
+            // Do nothing for iOS
+        }
 
-            function redirectBasedOnDevice() {
-                const deviceType = detectDevice();
-
-                if (deviceType === 'android') {
-                    // Attempt to open the app using a deep link
-                    // window.location.href = \`intent://#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=${encodeURIComponent(
-                      "https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik"
-                    )};end\`;
-                    // window.location.href = \`intent://co#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
-                    window.location.href = \`intent://video/${videoId}#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
-                    // window.location.href = "infotik.co://"; 
-                } else if (deviceType === 'web') {
-                    // Redirect web users to the website
-                    window.location.href = 'https://www.infotik.co';
-                }
-                // Do nothing for iOS
-            }
-
-            // Execute redirection logic
-            redirectBasedOnDevice();
-        </script>
+        // Execute redirection logic
+        redirectBasedOnDevice();
+      </script>
 
     </body>
 
@@ -138,67 +156,63 @@ app.get("/pulse/:pulseId", async (req, res) => {
     <html lang="en">
 
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${title}</title>
-        <meta property="og:title" content="${title}">
-        <meta property="og:description" content="${description}">
-          <meta property="og:image" content="${thumbnail}">
-          <meta property="og:url" content="https://${req.headers.host}/${pulseId}">
-          <meta property="og:type" content="pulse.other">
-        </head>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                background-color: #f5f5f5;
-            }
-        </style>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+      <meta property="og:title" content="${title}">
+      <meta property="og:description" content="${description}">
+        <meta property="og:image" content="${thumbnail}">
+        <meta property="og:url" content="https://${
+          req.headers.host
+        }/${pulseId}">
+        <meta property="og:type" content="pulse.other">
+      </head>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+              background-color: #f5f5f5;
+          }
+      </style>
     </head>
 
     <body>
-        Loading...
+      Loading...
 
-        <script>
-            function detectDevice() {
-                const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      <script>
+        function detectDevice() {
+          const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-                if (/android/i.test(userAgent)) {
-                    return 'android';
-                }
+          if (/android/i.test(userAgent)) {
+              return 'android';
+          }
 
-                if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-                    return 'ios';
-                }
+          if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+              return 'ios';
+          }
 
-                return 'web';
+          return 'web';
+        }
+
+        function redirectBasedOnDevice() {
+            const deviceType = detectDevice();
+
+            if (deviceType === 'android') {
+                window.location.href = \`intent://pulse/${pulseId}#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
+            } else if (deviceType === 'web') {
+                // Redirect web users to the website
+                window.location.href = 'https://www.infotik.co';
             }
+            // Do nothing for iOS
+        }
 
-            function redirectBasedOnDevice() {
-                const deviceType = detectDevice();
-
-                if (deviceType === 'android') {
-                    // Attempt to open the app using a deep link
-                    // window.location.href = \`intent://#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=${encodeURIComponent(
-                      "https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik"
-                    )};end\`;
-                    // window.location.href = \`intent://co#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
-                    window.location.href = \`intent://pulse/${pulseId}#Intent;scheme=infotik.co;package=com.zeeshan_raza.infotik;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.zeeshan_raza.infotik;end\`;
-                    // window.location.href = "infotik.co://"; 
-                } else if (deviceType === 'web') {
-                    // Redirect web users to the website
-                    window.location.href = 'https://www.infotik.co';
-                }
-                // Do nothing for iOS
-            }
-
-            // Execute redirection logic
-            redirectBasedOnDevice();
-        </script>
+        // Execute redirection logic
+        redirectBasedOnDevice();
+      </script>
 
     </body>
 
